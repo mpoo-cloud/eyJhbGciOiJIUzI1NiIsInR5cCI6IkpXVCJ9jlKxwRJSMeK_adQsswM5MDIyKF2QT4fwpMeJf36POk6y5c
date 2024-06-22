5f7431f697404e2f629d9be2f37a155a6c41e7d5
firsttraining <t9511015@gmail.com>
2024-06-22 12:39:02 -0400

    hello

---
+++ src/fastspin/fastspin.jsx
@@ -0,0 +1,19 @@
+import './fastspin.css';
+
+export const FastSpin = ()=>{
+    return(
+        <>
+
+
+        <div>
+            <div className='spinner'>
+
+            </div>
+            <p className='sam_abs' style={{color:'#blue', fontSize:'15.6px', wordSpacing:'1px',letterSpacing:'1px'}}>Setting up files now...</p>
+
+        </div>
+
+
+        </>
+    )
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Heyyy } from './heyyy';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('php'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Heyyy />
   </React.StrictMode>
 );
 
